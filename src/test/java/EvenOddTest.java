cf5a6eae065cdd4164a8b5e2cd5966c21d11c5da
Deepthi Pendekanti <pendekanti.sai2023@vitstudent.ac.in>
2026-03-05 16:32:08 +0530

    Create EvenOddTest for isEven method
    Add unit test for EvenOdd class to check isEven method.

---
+++ src/test/java/EvenOddTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class EvenOddTest {
+
+    @Test
+    void testMessage() {
+
+        assertEquals(
+            EvenOdd.isEven()
+        );
+
+    }
+
+}
